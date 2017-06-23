6feb7cdbadfce906cb2ad42a12f90abfef58d119
HighTech <htech.brasil@gmail.com>
2017-06-22 21:57:21 -0300

    Add

--- app/config.php
+++ app/config.php
@@ -4,6 +4,15 @@ $configs = new HXPHP\System\Configs\Config;
 
 $configs->env->add('development');
 
+$configs->env->development->baseURI = '/sistema_hx/';
+
+$configs->env->development->database->setConnectionData(array(
+  'host' => 'localhost',
+  'user' => 'root',
+  'password' => 'Abcde+12345',
+  'dbname' => 'sistemahx'
+));
+
 /*
   //Globais
   $configs->title = 'Titulo customizado';
