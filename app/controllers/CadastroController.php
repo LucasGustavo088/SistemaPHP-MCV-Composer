49df10b414f20b1b007a4eb1bd545eeea9abd434
HighTech <htech.brasil@gmail.com>
2017-06-23 22:59:45 -0300

    Inserção de comentários no controller cadastro e User

--- app/controllers/CadastroController.php
+++ app/controllers/CadastroController.php
@@ -2,32 +2,37 @@
 
 class CadastroController extends \HXPHP\System\Controller
 {
+	//configurando o login do usuário
 	public function __construct($configs){
 		parent::__construct($configs);
 
+		//carregando a autenticação do usuário da classe auth em services
 		$this->load(
 			'Services\Auth',
 			$configs->auth->after_login,
 			$configs->auth->after_logout,
 			true
 		);
-
+		//true: permite acessar cadastro mesmo logado | false: só permite cadastrar estando logado		
 		$this->auth->redirectCheck(true);
 	}
 
 	public function cadastrarAction()
 	{
+		//colocando este arquivo como o index
 		$this->view->setFile('index');
 
+		//validando o campo email e filtrando
 		$this->request->setCustomFilters(array(
 			'email' => FILTER_VALIDATE_EMAIL
 		));
-
+		//recebendo na variável $post um array de dados a serem inseridos no banco de dados
 		$post = $this->request->post();
-
+		//caso esteja com dados na variável $post, cadastre o usuário
 		if(!empty($post)){
+			//cadastrando o usuário com a classe user do active record
 			$cadastrarUsuario = User::cadastrar($post);
-
+			//caso ocorra algum erro de cadastro, exibir os erros através do framework HXPHP
 			if($cadastrarUsuario->status === false){
 				$this->load('Helpers\Alert', array(
 					'danger',
@@ -35,6 +40,7 @@ class CadastroController extends \HXPHP\System\Controller
 					$cadastrarUsuario->errors
 				));
 			} else {
+				//logar o usuário
 				$this->auth->login($cadastrarUsuario->user->id, $cadastrarUsuario->user->username);
 			}
 		}
--- app/models/User.php
+++ app/models/User.php
@@ -1,7 +1,9 @@
 <?php
 
 class User extends \HXPHP\System\Model
-{
+{	
+
+	//validação de dados com o active record
 	static $validates_presence_of = array(
 		array(
 			'name',
@@ -21,6 +23,7 @@ class User extends \HXPHP\System\Model
 		)
 	);
 
+	//veririficando se ja consta o username ou email inserido no banco de dados com o active record
 	static $validates_uniqueness_of = array(
 		array(
 			array('username', 'email'),
@@ -28,31 +31,36 @@ class User extends \HXPHP\System\Model
 		)
 	);
 
+	//método pra cadastrar o usuário através do cadastro controller
 	public static function cadastrar(array $post)
 	{
+		//criando um objeto nulo para cadastrar o úsuário
 		$callbackObj = new \stdClass;
 		$callbackObj->user = null;
 		$callbackObj->status = false;
 		$callbackObj->errors = array();
-
+		//encontrar atributo(user) de uma tabela
 		$role = Role::find_by_role('user');
 
+		//caso não ache o atributo, retornar a mensagem
 		if(is_null($role)){
 			array_push($callbackObj->errors, 'A role user não existe. Contate o administrador');
 			return $callbackObj;
 		}
 
+		//concatenando os dados do usuário com dados extras pra formar a tupla completa numa variavel
+		//de dados do usuário
 		$user_data = array_merge($post, array(
 			'role_id' => $role->id,
 			'status' => 1
 		));
-
+		//criprografando a senha
 		$password = \HXPHP\System\Tools::hashHX($post['password']);
-
+		//concatenando a variável $post com os dados com a senha pra inserir no banco de dados
 		$post = array_merge($user_data, $password);
-
+		//inserindo o usuário no banco de dados
 		$cadastrar = self::create($post);
-
+		//verificando se o cadastro foi valido
 		if($cadastrar->is_valid()){
 			$callbackObj->user = $cadastrar;
 			$callbackObj->status = true;
@@ -60,7 +68,7 @@ class User extends \HXPHP\System\Model
 		}
 
 		$errors = $cadastrar->errors->get_raw_errors();
-
+		//criando um array list pra retornar todos os erros que ocorreram na inserção dos dados do usuário no banco
 		foreach($errors as $field => $message){
 			array_push($callbackObj->errors, $message[0]);
 		}
